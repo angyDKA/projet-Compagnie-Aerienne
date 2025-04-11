746617ab79526dec34ccc375388d21e49652e22e
Nolwen KENMOE <angenolwen@gmail.com>
2025-04-11 10:51:21 +0200

    nouveau commit

---
+++ src/main/java/app/Employe.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Employe extends Personne{
+    private String numeroEmploye;
+    private String dateEmbauche;
+
+    public Employe (String identifiant, String nom, String adresse, String contact)
+        super (identifiant, nom, adresse, contact);
+
+}
