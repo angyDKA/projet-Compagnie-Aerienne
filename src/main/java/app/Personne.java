843af95e43efd5295facfa755224c02e7ba40bc5
rem911 <simsremy71@gmail.com>
2025-04-11 11:23:30 +0200

    ajout de la classe Personne.java

--- src/main/java/app/Personne.java
+++ src/main/java/app/Personne.java
@@ -5,19 +5,13 @@ public class Personne {
     private String nom;
     private String adresse;
     private String contact;
-     public Personne(String identifiant,String nom,String adresse, String contact){
+
+    public Personne(String identifiant,String nom,String adresse, String contact){
          this.identifiant= identifiant;
          this.nom=nom;
          this.adresse=adresse;
          this.contact=contact;
      }
-     public void obtenirInfos(){
-            System.out.println("ID:"+ identifiant);
-            System.out.println("Nom:" + nom);
-            System.out.println("Adresse:" + adresse);
-            System.out.println("Contact:" + contact);
-     }
-
     public String getIdentifiant() {
         return identifiant;
     }
@@ -48,6 +42,12 @@ public class Personne {
 
     public void setContact(String contact) {
         this.contact = contact;
+    }
+    public void obtenirInfos(){
+        System.out.println("ID:"+ identifiant);
+        System.out.println("Nom:" + nom);
+        System.out.println("Adresse:" + adresse);
+        System.out.println("Contact:" + contact);
     }
 }
 
