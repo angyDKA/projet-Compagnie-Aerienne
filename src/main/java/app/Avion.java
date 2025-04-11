1675b51e6aebc4bcfaf021153f8eae4bbea8a0b5
rem911 <simsremy71@gmail.com>
2025-04-11 11:54:51 +0200

    ajout de la classe Avion.java

---
+++ src/main/java/app/Avion.java
@@ -0,0 +1,37 @@
+package app;
+
+public class Avion {
+    private String immatriculation;
+    private String modele;
+    private int capacite;
+    public Avion(String immatriculation, String modele, int capacite){
+        this.immatriculation= immatriculation;
+        this.modele= modele;
+        this.capacite= capacite;
+    }
+
+    public String getImmatriculation() {
+        return immatriculation;
+    }
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
+    }
+    public String getModele() {
+        return modele;
+    }
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+    public int getCapacite() {
+        return capacite;
+    }
+    public void setCapacite(int capacite) {
+        this.capacite = capacite;
+    }
+    public void affecterVol(){
+
+    }
+    public void verifierDispoAvion(){
+
+    }
+}
